5c1706543df43f5883c4c3f4ee712024d2756df4
hidden-dimensions <20926615+hidden-dimensions@users.noreply.github.com>
2019-08-09 17:13:02 +0800

    skeleton server

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # presentation-microservices-kotlin
 Presentation code and material for micro-services with kotlin
+
+## Get Started
+##### Start the server
+```
+cd backend
+./gradlew bootRun
+```
+##### Testing api call
+```
+curl http://localhost:8080/ping
+```
---
+++ backend/src/main/kotlin/com/oursky/presentation/microservices/kotlin/todo/ToDoApplication.kt
@@ -0,0 +1,11 @@
+package com.oursky.presentation.microservices.kotlin.todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToDoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToDoApplication>(*args)
+}
