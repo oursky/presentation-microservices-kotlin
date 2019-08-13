29183d57b3b213da96b88a271394b9f2ee5a1463
hidden-dimensions <20926615+hidden-dimensions@users.noreply.github.com>
2019-08-13 16:06:06 +0800

    setup ktlint #4

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -7,15 +7,20 @@ plugins {
 	kotlin("plugin.spring") version "1.2.71"
 }
 
+repositories {
+	mavenCentral()
+	jcenter()
+}
+
 group = "com.oursky.presentation.microservices.kotlin"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-repositories {
-	mavenCentral()
-}
+val ktlint by configurations.creating
 
 dependencies {
+	ktlint("com.github.shyiko:ktlint:0.31.0")
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -28,4 +33,24 @@ tasks.withType<KotlinCompile> {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "1.8"
 	}
+}
+
+tasks.register<JavaExec>("ktlint") {
+    group = "verification"
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    main = "com.github.shyiko.ktlint.Main"
+    args("--android", "src/**/*.kt")
+}
+
+tasks.named("check") {
+    dependsOn(ktlint)
+}
+
+tasks.register<JavaExec>("ktlintFormat") {
+    group = "formatting"
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    main = "com.github.shyiko.ktlint.Main"
+    args("--android", "-F", "src/**/*.kt")
 }
