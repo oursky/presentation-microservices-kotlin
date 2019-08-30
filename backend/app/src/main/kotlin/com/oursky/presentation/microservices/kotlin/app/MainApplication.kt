8e239e8341fff78d3462564d2f3437bf28df3ec9
hidden-dimensions <20926615+hidden-dimensions@users.noreply.github.com>
2019-08-30 14:49:49 +0800

    app microservice skeleton #11

---
+++ backend/app/src/main/kotlin/com/oursky/presentation/microservices/kotlin/app/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.oursky.presentation.microservices.kotlin.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MainApplication
+
+fun main(args: Array<String>) {
+    runApplication<MainApplication>(*args)
+}
