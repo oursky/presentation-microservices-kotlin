97bf2caeb140007283a22ef118221d75dc75c523
MartinLam-Oursky <martinlam@oursky.com>
2019-09-11 12:23:52 +0800

    Updated Form Submission Handler

--- frontend/src/Components/NewProduct.tsx
+++ frontend/src/Components/NewProduct.tsx
@@ -1,61 +1,34 @@
 import React from "react";
 // import { render } from "react-dom";
 import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input'
+import  { Redirect } from 'react-router-dom'
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid'
 
 export default class NewProduct extends React.Component {
-    state = ({
-        success: false,
-        url: ""
-    })
 
     handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         var form = document.querySelector("form");
         if( !form ) return false;
         var formData = new FormData(form);
-        var formDataObj = Object.fromEntries(formData);
-        
+
         fetch("http://127.0.0.1:8080/product/", {
             method: "POST",
-            body: JSON.stringify(formDataObj),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
+            body: formData
         }).then(async v => {
-            const { productId } = await v.json();
-            const image = formData.get("files") as File;
-            console.log(image.type, image.size); //image/jpeg 11734
-            formData.delete("name");
-            formData.delete("description");
-            formData.delete("price");
-            if(image.size) {
-                console.log("Product Image Found, uploading...")
-                try{
-                    const result = await fetch(`http://127.0.0.1:8080/product/${productId}/image`, {
-                        method: "POST",
-                        body: formData
-                    });
-                    console.log("Image Upload Result: ", await result.json())
-                }catch(e){
-                    console.log("Error in Image Upload: ", e);
-                }
-            }
-            console.log("Done, Product ID: ", productId);
+            const {productId} = await v.json();
+            alert(`New Prodcut Added !\nProduct ID: ${productId}`);
         }).catch(e => {
-            console.log("Error: ", e)
+            alert(`Error: ${e}`)
         })
     }
 
-    
-
     render(){
         return (
+
             <div className = "productSubmitDiv">
                 <Grid
                     container
@@ -69,9 +42,7 @@ export default class NewProduct extends React.Component {
                     <Typography variant="h4" component="h4">
                         Add New Product
                     </Typography>
-                    {/* onSubmit = {this.handleFormSubmit} */}
-                    {/* action = "http://localhost:8080/product/" method = "post" */}
-                    <form id = "addProductForm" onSubmit = {this.handleFormSubmit}>
+                    <form id = "addProductForm" onSubmit = {this.handleFormSubmit} >
                         <fieldset>
                             <TextField
                                 required
@@ -109,13 +80,7 @@ export default class NewProduct extends React.Component {
                                 name = "files"
                                 id = "raised-button-file"
                                 accept = "image/*"
-                                // style = {{display: "none"}}
                             />
-                            {/* <label htmlFor="raised-button-file"> 
-                                <Button fullWidth component = "span" variant = "outlined" color = "secondary"> 
-                                    Upload Product Image
-                                </Button> 
-                            </label>  */}
                         </fieldset>
                         <br />
                         <br />
