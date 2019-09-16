7e8c3e51ee247e31c0dc476746b27ea3dbbe28f3
MartinLam-Oursky <martinlam@oursky.com>
2019-09-16 17:07:22 +0800

    Add login page & route

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Route, Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
-/**
- * Import all page components here
- */
 import App from './Components/App';
 import Navbar from './Components/Navbar';
 import NewProductPage from './Components/NewProduct';
 import ShoppingCart from './Components/ShoppingCart';
+import Login from './Components/Login';
 
 const history = createBrowserHistory();
 
-/**
- * All routes go here.
- * Don't forget to import the components above after adding new route.
- */
 export default (
     <Router history = {history}>
         <Navbar/>
@@ -22,5 +16,6 @@ export default (
         <Route path = "/" exact component = {App} />
         <Route path = "/new" exact component = {NewProductPage} />
         <Route path = "/cart" exact component = {ShoppingCart} />
+        <Route path = "/login" exact component = {Login} />
     </Router>
 );
