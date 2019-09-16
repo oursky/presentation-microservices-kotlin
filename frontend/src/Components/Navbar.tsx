f5dcfd04e1b8b37441d8791a76481d8afcdd1ba9
MartinLam-Oursky <martinlam@oursky.com>
2019-09-16 16:47:19 +0800

    Update navbar

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,41 +1,147 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Menu,
+    MenuItem,
+} from '@material-ui/core';
+import { MenuProps } from '@material-ui/core/Menu';
 import { Link } from "react-router-dom";
+import {
+    AccountCircleRounded,
+    ShoppingCartRounded
+} from '@material-ui/icons';
+import MenuIcon from '@material-ui/icons/Menu'
+import { createStyles, makeStyles, Theme, withStyles } from '@material-ui/core/styles';
 
-export default class Navbar extends React.Component {
+const StyledMenu = withStyles({
+    paper: {
+        border: '1px solid #d3d4d5',
+    },
+})((props: MenuProps) => (
+    <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+        }}
+        { ...props }
+    />
+));
 
-    render(){
-        return (
-            <div>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6">
-                            E-Commerce
-                        </Typography>
-                        
-                        <MenuItem>
-                            <Link to = "/" style = {{ textDecoration: 'none', color: "white" }}>Products</Link>
-                        </MenuItem>
-                    
-                        <MenuItem>
-                            <Link to = "/new" style = {{ textDecoration: 'none', color: "white" }}>Add Products</Link>
-                        </MenuItem>
+const StyledMenuItem = withStyles(theme => ({
+    root: {
+        '&:focus': {
+            backgroundColor: theme.palette.primary.main,
+            '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+                color: theme.palette.common.white,
+            },
+        },
+    },
+}))(MenuItem);
 
-                        <MenuItem>
-                            <Link to = "/cart" style = {{ textDecoration: 'none', color: "white" }}>Shopping Cart</Link>
-                        </MenuItem>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
 
-                    </Toolbar>
-                </AppBar>
-            </div>
-        )
+    root: {
+         flexGrow: 1,
+    },
+
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+
+    menuItems: {
+        marginLeft: 10,
+        marginRight: 'auto',
+    },
+
+    menuItem: {
+        display: 'inline-block'
+    },
+
+    menuItemText:{
+        display: 'inline-block',
+        paddingTop: theme.spacing(1)
     }
+  }),
+);
+
+export default function Navbar(){
+
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    function handleClick(event: React.MouseEvent<HTMLElement>) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    function handleClose() {
+        setAnchorEl(null);
+    }
+
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+
+                    <Typography variant="h6" color = "inherit">
+                        E-Commerce
+                    </Typography>
+                    
+                    <section className = {classes.menuItems}>
+                        <MenuItem className = {classes.menuItem}>
+                            <Link className = {classes.menuItemText} to = "/" style = {{ textDecoration: 'none', color: "white" }}>Products</Link>
+                        </MenuItem>
+
+                        <MenuItem className = {classes.menuItem}>
+                            <Link className = {classes.menuItemText} to = "/new" style = {{ textDecoration: 'none', color: "white" }}>Add Products</Link>
+                        </MenuItem>
+
+                    </section>
+                    
+                    <MenuItem>
+                        <Link className = {classes.menuItemText} to = "/cart" style = {{ textDecoration: 'none', color: "white" }}>
+                            <ShoppingCartRounded />
+                        </Link>
+                    </MenuItem>
+
+                    <MenuItem aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                        <AccountCircleRounded />
+                    </MenuItem>
+
+
+
+                </Toolbar>
+            </AppBar>
+            <StyledMenu
+                id="customized-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <StyledMenuItem onClick={handleClose}>
+                    <Link to = "/login" style = {{ textDecoration: 'none', color: "black" }}>
+                        Login
+                     </Link>
+                </StyledMenuItem>
+                <StyledMenuItem onClick={handleClose}>
+                    <Link to = "/register" style = {{ textDecoration: 'none', color: "black" }}>
+                        Register
+                     </Link>
+                </StyledMenuItem>
+            </StyledMenu>
+        </div>
+    )
 }
