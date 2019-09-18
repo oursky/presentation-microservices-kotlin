7773bec64ed138e7066adbe48b92719bcc310df1
MartinLam-Oursky <martinlam@oursky.com>
2019-09-18 18:19:16 +0800

    Update new product page to functional component

--- frontend/src/Components/NewProduct.tsx
+++ frontend/src/Components/NewProduct.tsx
@@ -1,59 +1,74 @@
 import React from "react";
+import {useState} from "react";
 import APIService from "../APIService"
 import { TextField, InputAdornment, Button, Typography, Grid } from '@material-ui/core';
+import PopupBox from "./PopupBox";
 
-export default class NewProduct extends React.Component {
+export default function NewProduct(){
 
-    handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+    const [submitResult, setSubmitResult] = useState("")
+
+    function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        var form = document.querySelector("form");
+        const form = document.querySelector("form");
         if( !form ) return false;
 
         APIService.Products.add(new FormData(form))
         .then( productId => {
-            alert(`New Prodcut Added !\nProduct ID: ${productId}`);
+            setSubmitResult(`New Prodcut Added !\nProduct ID: ${productId}`)
         })
         .catch(err => {
-            alert(`Error: ${err}`)
+            setSubmitResult(`Error: ${err}`)
         })
     }
 
-    render(){
-        return (
+    return (
+        <div>
 
-            <div className = "productSubmitDiv">
-                <Grid
-                    container
-                    spacing={0}
-                    direction="column"
-                    alignItems="center"
-                    style={{ minHeight: '100vh' }}
-                >
+            {
+                submitResult === "" ? "" : <PopupBox title = "Submit Result" message = {submitResult}/>
+            }
+
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                style={{ minHeight: '100vh' }}
+            >
 
                 <Grid item xs={4}>
+
                     <Typography variant="h4" component="h4">
                         Add New Product
                     </Typography>
-                    <form id = "addProductForm" onSubmit = {this.handleFormSubmit} >
+
+                    <form id = "addProductForm" onSubmit = {handleFormSubmit} >
                         <fieldset>
+
                             <TextField
                                 required
                                 fullWidth
                                 id = "productName"
                                 name = "name"
                                 variant = "outlined"
-                                label = "Product Name" />
+                                label = "Product Name" 
+                            />
+
                             <br />
                             <br />
+
                             <TextField
-                                
                                 fullWidth
                                 id = "productDescription"
                                 name = "description"
                                 variant = "outlined"
-                                label = "Product Description" />
+                                label = "Product Description" 
+                            />
+
                             <br />
                             <br />
+
                             <TextField
                                 required
                                 fullWidth
@@ -64,27 +79,35 @@ export default class NewProduct extends React.Component {
                                 type = "number"
                                 InputProps={{
                                     startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                                }}/>
+                                }}
+                            />
+
                             <br />
                             <br />
+
                             <input
                                 type = "file"
                                 name = "files"
                                 id = "raised-button-file"
                                 accept = "image/*"
                             />
+
                         </fieldset>
+
                         <br />
                         <br />
+
                         <Button fullWidth variant = "outlined" color = "primary" type = "submit">
                             Submit
                         </Button>
                         
                     </form>
-                </Grid>   
-                    
-                </Grid> 
-            </div>
-        )
-    }
+
+                </Grid>  
+
+            </Grid> 
+            
+        </div>
+    )
+
 }
