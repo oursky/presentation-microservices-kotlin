2bc44a4abba5031550f9857f37f90bacbd597b76
MartinLam-Oursky <martinlam@oursky.com>
2019-09-18 18:34:06 +0800

    Update register page to functional component

--- frontend/src/Components/Register.tsx
+++ frontend/src/Components/Register.tsx
@@ -1,98 +1,105 @@
 import React from "react";
-import APIService from "../APIService"
+import {useState} from "react";
+import APIService from "../APIService";
 import { TextField, Button, Typography, Grid } from '@material-ui/core';
+import PopupBox from "./PopupBox";
 
-export default class Register extends React.Component{
-    handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+export default function Register(){
+
+    const [submitResult, setSubmitResult] = useState("")
+
+    function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const form = document.querySelector("form");
         if( !form ) return false;
         const formData = new FormData(form);
 
         if(formData.get("confpass") !== formData.get("pass")){
-            alert("Passwords are not the same !")
+            setSubmitResult("Passwords are not the same !")
             return;
         }
 
         APIService.User.register(formData)
         .then(result => {
             console.log(result);
-            alert("Done")
+            setSubmitResult("Successfully Registered.")
         })
         .catch(e => {
-            alert(`Error: ${e}`)
+            setSubmitResult(`Error: ${e}`)
         })
     }
 
-    render(){
-        return (
+    return (
+        <div>
 
-            <div>
-                <Grid
-                    container
-                    spacing={0}
-                    direction="column"
-                    alignItems="center"
-                    style={{ minHeight: '100vh' }}
-                >
+            {
+                submitResult === "" ? "" : <PopupBox title = "Registeration Result" message = {submitResult} />
+            }
 
-                    <Grid item xs={4}>
-                        <Typography variant="h4" component="h4">
-                            Register
-                        </Typography>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                style={{ minHeight: '100vh' }}
+            >
+
+                <Grid item xs={4}>
+                    <Typography variant="h4" component="h4">
+                        Register
+                    </Typography>
+
+                    <br />
+
+                    <form onSubmit = {handleFormSubmit} >
+
+                        <fieldset>
+
+                            <TextField
+                                required
+                                fullWidth
+                                name = "user"
+                                variant = "outlined"
+                                label = "Username" 
+                            />
+
+                            <br />
+                            <br />
+
+                            <TextField
+                                fullWidth
+                                required
+                                name = "pass"
+                                type = "password"
+                                variant = "outlined"
+                                label = "Password" 
+                            />
+
+                            <br />
+                            <br />
+
+                            <TextField
+                                fullWidth
+                                required
+                                name = "confpass"
+                                type = "password"
+                                variant = "outlined"
+                                label = "Confirm Password" 
+                            />
+
+                        </fieldset>
 
                         <br />
+                        <br />
 
-                        <form onSubmit = {this.handleFormSubmit} >
-
-                            <fieldset>
-
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name = "user"
-                                    variant = "outlined"
-                                    label = "Username" 
-                                />
-
-                                <br />
-                                <br />
-
-                                <TextField
-                                    fullWidth
-                                    required
-                                    name = "pass"
-                                    type = "password"
-                                    variant = "outlined"
-                                    label = "Password" 
-                                />
-
-                                <br />
-                                <br />
-
-                                <TextField
-                                    fullWidth
-                                    required
-                                    name = "confpass"
-                                    type = "password"
-                                    variant = "outlined"
-                                    label = "Confirm Password" 
-                                />
-
-                            </fieldset>
-
-                            <br />
-                            <br />
-
-                            <Button fullWidth variant = "outlined" color = "primary" type = "submit">
-                                Submit
-                            </Button>
-                            
-                        </form>
-                    </Grid>   
-                    
-                </Grid> 
-            </div>
-        )
-    }
+                        <Button fullWidth variant = "outlined" color = "primary" type = "submit">
+                            Submit
+                        </Button>
+                        
+                    </form>
+                </Grid>   
+                
+            </Grid> 
+        </div>
+    )
 }
