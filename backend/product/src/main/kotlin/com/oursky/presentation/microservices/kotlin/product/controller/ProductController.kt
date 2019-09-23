c1a5b13c2adb60d87ab7f92589249e25a3eac768
MartinLam-Oursky <martinlam@oursky.com>
2019-09-23 18:30:23 +0800

    Remove wildcard import

--- backend/product/src/main/kotlin/com/oursky/presentation/microservices/kotlin/product/controller/ProductController.kt
+++ backend/product/src/main/kotlin/com/oursky/presentation/microservices/kotlin/product/controller/ProductController.kt
@@ -8,7 +8,14 @@ import com.oursky.presentation.microservices.kotlin.product.entity.Product
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.multipart.MultipartHttpServletRequest
 import com.oursky.presentation.microservices.kotlin.product.service.JwtService
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.DeleteMapping
 
 @RestController
 @RequestMapping("/product")
