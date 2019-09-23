900e8b61b02f558a9da6d395657ec6f0f7ae0ddd
MartinLam-Oursky <martinlam@oursky.com>
2019-09-23 18:08:42 +0800

    Fix missing import

--- backend/auth/src/main/kotlin/com/oursky/presentation/microservices/kotlin/auth/controller/AuthController.kt
+++ backend/auth/src/main/kotlin/com/oursky/presentation/microservices/kotlin/auth/controller/AuthController.kt
@@ -1,16 +1,11 @@
 package com.oursky.presentation.microservices.kotlin.auth.controller
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import com.oursky.presentation.microservices.kotlin.auth.service.AuthService
 import com.oursky.presentation.microservices.kotlin.auth.service.JwtService
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/auth")
