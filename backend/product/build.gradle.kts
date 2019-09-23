a60da6bfb01f5ee7534cc1fd3816249e55bb4a32
MartinLam-Oursky <martinlam@oursky.com>
2019-09-23 18:21:22 +0800

    Fix JwtService & import missing

--- backend/product/build.gradle.kts
+++ backend/product/build.gradle.kts
@@ -20,6 +20,7 @@ val ktlint by configurations.creating
 
 dependencies {
 	ktlint("com.github.shyiko:ktlint:0.31.0")
+	implementation("com.auth0:java-jwt:3.8.1")
 	implementation("io.minio:minio:6.0.10")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
--- backend/product/src/main/kotlin/com/oursky/presentation/microservices/kotlin/product/controller/ProductController.kt
+++ backend/product/src/main/kotlin/com/oursky/presentation/microservices/kotlin/product/controller/ProductController.kt
@@ -1,25 +1,23 @@
 package com.oursky.presentation.microservices.kotlin.product.controller
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import com.oursky.presentation.microservices.kotlin.product.service.ProductService
 import com.oursky.presentation.microservices.kotlin.product.entity.Product
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.multipart.MultipartHttpServletRequest
+import com.oursky.presentation.microservices.kotlin.product.service.JwtService
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
     @Autowired
     lateinit var productService: ProductService
+    @Autowired
+    lateinit var jwtService: JwtService
 
     data class AddProductResponse(
         val productId: Long?,
@@ -41,7 +39,8 @@ public class ProductController {
         @RequestParam("files") image: MultipartFile,
         @RequestParam("name") name: String,
         @RequestParam("description") description: String,
-        @RequestParam("price") price: Float
+        @RequestParam("price") price: Float,
+        @RequestHeader("authorization") authorization: String
     ): ResponseEntity<AddProductResponse> {
 
         val jwt = authorization.replace("Bearer ", "", true)
@@ -71,7 +70,8 @@ public class ProductController {
 
     @DeleteMapping("/{id}")
     fun deleteProduct(
-        @PathVariable id: Long
+        @PathVariable id: Long,
+        @RequestHeader("authorization") authorization: String
     ): ResponseEntity<DeleteProductResponse> {
 
         val jwt = authorization.replace("Bearer ", "", true)
