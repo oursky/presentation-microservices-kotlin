7dbac787c8b059b8115d6b567e7a623aba39e51b
MartinLam-Oursky <martinlam@oursky.com>
2019-09-23 18:28:41 +0800

    Remove wildcard import

--- backend/auth/src/main/kotlin/com/oursky/presentation/microservices/kotlin/auth/controller/AuthController.kt
+++ backend/auth/src/main/kotlin/com/oursky/presentation/microservices/kotlin/auth/controller/AuthController.kt
@@ -5,7 +5,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import com.oursky.presentation.microservices.kotlin.auth.service.AuthService
 import com.oursky.presentation.microservices.kotlin.auth.service.JwtService
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 
 @RestController
 @RequestMapping("/auth")
