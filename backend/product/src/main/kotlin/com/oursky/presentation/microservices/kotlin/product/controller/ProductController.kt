0881c98d60735757625a24e7957af6109c0bb6e8
MartinLam-Oursky <martinlam@oursky.com>
2019-09-23 18:39:38 +0800

    Remove product controller wildcard import

--- backend/product/src/main/kotlin/com/oursky/presentation/microservices/kotlin/product/controller/ProductController.kt
+++ backend/product/src/main/kotlin/com/oursky/presentation/microservices/kotlin/product/controller/ProductController.kt
@@ -5,7 +5,14 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import com.oursky.presentation.microservices.kotlin.product.service.ProductService
 import com.oursky.presentation.microservices.kotlin.product.entity.Product
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.multipart.MultipartHttpServletRequest
 
